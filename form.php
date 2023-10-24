cc385fcbd61420be971d7e57179bf124745cadbe
Amanuel-JuniorT <amanueltzb@gmail.com>
2023-10-24 00:30:14 -0800

    edit overall

--- form.php
+++ form.php
@@ -1,17 +1,18 @@
 <?php
-$dom = new DOMDocument();
-$dom->load('./form.php');
-
-  $firstName = $_POST['firstName'];
+$firstName = $_POST['firstName'];
   $lastName = $_POST['lastName'];
   $email = $_POST['email'];
   $password = $_POST['password'];
-  $num = $_POST['phone'];
-  echo $status = $dom->getElementById('status')->tagName;
-  
-  //$firstName = $_POST['firstName'];
+  $phone = $_POST['phone'];
+  $body = '';
 
+  $to = "amanueltzb@gmail.com";
+  $headers = "From: " . $email . "\r\n";
+  $body .= "Name: $firstName $lastName \r\n";
+  $body .= "Phone: $phone \r\n";
+  //$body .= "Name: $firstName $lastName \r\n";
   
+  //mail($to, $headers, $body);
 
 ?>
 
@@ -42,23 +43,23 @@ $dom->load('./form.php');
         <label for="email">E-Mail</label>
         <input type="email" id="email" name="email" required autocomplete="email">  
       </div>
-      <div>
+      <!-- <div>
         <label for="password">Password</label>
         <input type="password" id="password" name="password" required>  
-      </div>
+      </div> -->
       <div>
         <label for="phone">Phone</label>
         <input type="tel" id="phone" name="phone" required autocomplete="mobile">  
       </div>
       <div class="apply">
         <label for="status">Apply for</label>
-        <!-- <select name="status" id="status">
+        <select name="status" id="status">
           <option value="">Hello</option>
           <option value="">Hello</option>
           <option value="">Hello</option>
           <option value="">Hello</option>
-        </select> -->
-        <div class="status" id="status">-Select-<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-down-fill" viewBox="0 0 16 16">
+        </select>
+        <!-- <div class="status">-Select-<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-down-fill" viewBox="0 0 16 16">
           <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z"/>
         </svg></div>
         <ul id="status">
@@ -66,7 +67,7 @@ $dom->load('./form.php');
           <li class="dep">Masters</li>
           <li class="dep">Internships</li>
           <li class="dep">Training</li>
-        </ul>
+        </ul> -->
       </div>
       <div>
         <label for="file">Transcript 9-12</label>
